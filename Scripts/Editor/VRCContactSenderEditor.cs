ad645f3d4c7c79c08e12ef96501f5f0fed936408
Tayou <tayou@gmx.net>
2022-04-29 17:37:34 +0200

    adding package manifest

---
+++ Scripts/Editor/VRCContactSenderEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRCSDK.
+
+public class VRCContactSenderCommunityEditor : VRCContactSenderEditor
+{
+    SerializedProperty targetObject;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        targetObject = (VRCContectSender)serializedObject;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.Space();
+
+        serializedObject.Update();
+        //EditorGUILayout.PropertyField(m_testObject);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
